bfc5cf68303e39255cd4c26eb5b1ae2b41314c00
vsilvalopes <vsilvalopes@gmail.com>
2015-08-04 23:53:28 -0300

    initial commit

---
+++ WindowsApplication1A/Form1.vb
@@ -0,0 +1,78 @@
+﻿Imports System.Xml
+Public Class Form1
+
+    Dim MediaCenter As Object
+    Dim GameCenter As Object
+    Dim Steam As Object
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        Button1.BackgroundImage = My.Resources.xbmc_off
+        Button1.BackgroundImageLayout = ImageLayout.Stretch
+
+        Button2.BackgroundImage = My.Resources.steam_off
+        Button2.BackgroundImageLayout = ImageLayout.Stretch
+
+        Button3.BackgroundImage = My.Resources.gameex_off
+        Button3.BackgroundImageLayout = ImageLayout.Stretch
+
+        Dim xmlDoc As XmlDocument = New XmlDocument
+        xmlDoc.Load(Application.StartupPath + "\config.xml")
+
+        Dim Principal = xmlDoc.DocumentElement
+
+        MediaCenter = Principal("MediaCenter").InnerText
+        GameCenter = Principal("GameCenter").InnerText
+        Steam = Principal("Steam").InnerText
+
+    End Sub
+
+    Private Sub Button1_GotFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button1.GotFocus
+        Button1.BackgroundImage = My.Resources.xbmc_on
+        Button1.BackgroundImageLayout = ImageLayout.Stretch
+    End Sub
+
+    Private Sub Button1_LostFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button1.LostFocus
+        Button1.BackgroundImage = My.Resources.xbmc_off
+        Button1.BackgroundImageLayout = ImageLayout.Stretch
+    End Sub
+
+    Private Sub Button2_GotFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button2.GotFocus
+        Button2.BackgroundImage = My.Resources.steam_on
+        Button2.BackgroundImageLayout = ImageLayout.Stretch
+    End Sub
+
+    Private Sub Button2_LostFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button2.LostFocus
+        Button2.BackgroundImage = My.Resources.steam_off
+        Button2.BackgroundImageLayout = ImageLayout.Stretch
+    End Sub
+
+    Private Sub Button3_GotFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button3.GotFocus
+        Button3.BackgroundImage = My.Resources.gameex_on
+        Button3.BackgroundImageLayout = ImageLayout.Stretch
+    End Sub
+
+    Private Sub Button3_LostFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button3.LostFocus
+        Button3.BackgroundImage = My.Resources.gameex_off
+        Button3.BackgroundImageLayout = ImageLayout.Stretch
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim dblRetVal As Double
+        dblRetVal = Shell(MediaCenter, vbMaximizedFocus)
+        End
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim dblRetVal As Double
+        dblRetVal = Shell(Steam, vbMaximizedFocus)
+        End
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Dim dblRetVal As Double
+        dblRetVal = Shell(GameCenter, vbMaximizedFocus)
+        End
+    End Sub
+End Class
---
+++ WindowsApplication1A/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("WindowsApplication1A")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Microsoft")> 
+<Assembly: AssemblyProduct("WindowsApplication1A")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2010")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("86c02eb1-bc2c-4654-9178-7b65b361494a")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
